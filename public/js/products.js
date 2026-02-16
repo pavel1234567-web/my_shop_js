7b252a1f32e07f3c4045021340dcbacc0578e582
Ваше Имя <.email@example.com>
2026-02-16 17:36:04 +0200

    auto_commit 16-02-2026_17-36

--- public/js/products.js
+++ public/js/products.js
@@ -40,6 +40,97 @@ const products = [
     "price": 400.0,
     "article": "150",
     "image": "public/img/products/vlcsnap-2024-09-11-18h40m58s850.png"
+  },
+  {
+    "id": 7,
+    "name": "Товар 1",
+    "price": 100.0,
+    "article": "ART-001",
+    "image": "public/img/products/20180915_113204_000019_qTYXR9N.png"
+  },
+  {
+    "id": 8,
+    "name": "Товар 2",
+    "price": 250.0,
+    "article": "ART-002",
+    "image": "no-image.png"
+  },
+  {
+    "id": 9,
+    "name": "Товар 3",
+    "price": 50.0,
+    "article": "ART-003",
+    "image": "no-image.png"
+  },
+  {
+    "id": 10,
+    "name": "Товар 4",
+    "price": 150.0,
+    "article": "ART-004",
+    "image": "no-image.png"
+  },
+  {
+    "id": 11,
+    "name": "Товар 5",
+    "price": 300.0,
+    "article": "ART-005",
+    "image": "no-image.png"
+  },
+  {
+    "id": 12,
+    "name": "Товар 6",
+    "price": 120.0,
+    "article": "ART-006",
+    "image": "no-image.png"
+  },
+  {
+    "id": 13,
+    "name": "Товар 7",
+    "price": 220.0,
+    "article": "ART-007",
+    "image": "no-image.png"
+  },
+  {
+    "id": 14,
+    "name": "Товар 8",
+    "price": 80.0,
+    "article": "ART-008",
+    "image": "no-image.png"
+  },
+  {
+    "id": 15,
+    "name": "Товар 9",
+    "price": 450.0,
+    "article": "ART-009",
+    "image": "no-image.png"
+  },
+  {
+    "id": 16,
+    "name": "Товар 10",
+    "price": 60.0,
+    "article": "ART-010",
+    "image": "no-image.png"
+  },
+  {
+    "id": 17,
+    "name": "товар очень  хороший",
+    "price": 100.0,
+    "article": "450",
+    "image": "public/img/products/vlcsnap-2024-09-11-18h39m53s302_bsFxiXe.png"
+  },
+  {
+    "id": 18,
+    "name": "новый товар",
+    "price": 70.0,
+    "article": "500",
+    "image": "public/img/products/vlcsnap-2024-09-11-18h51m58s188_tlnJkNF.png"
+  },
+  {
+    "id": 19,
+    "name": "нужный товар",
+    "price": 90.0,
+    "article": "90",
+    "image": "public/img/products/20180915_113204_000019_xw8atsH.png"
   }
 ];
 
