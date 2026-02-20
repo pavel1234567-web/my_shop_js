92f1d4bf4ff863a86fc41849480e7c3dee27cb08
Ваше Имя <.email@example.com>
2026-02-20 02:54:19 +0200

    auto_commit 20-02-2026_2-54

--- public/js/products.js
+++ public/js/products.js
@@ -1,136 +1,66 @@
 const products = [
   {
-    "id": 1,
-    "name": "товар",
-    "price": 150.0,
-    "article": "50",
-    "image": "public/img/products/20180915_113204_000019_vGl2JXH.png"
-  },
-  {
-    "id": 2,
-    "name": "товар1",
-    "price": 200.0,
-    "article": "60",
-    "image": "public/img/products/20180915_113204_000019_wEEAFLV.png"
-  },
-  {
-    "id": 3,
-    "name": "товар 3",
-    "price": 250.0,
-    "article": "70",
-    "image": "public/img/products/vlcsnap-2024-09-11-18h39m53s302_UaZyUyO.png"
-  },
-  {
-    "id": 4,
-    "name": "товар 4",
-    "price": 300.0,
-    "article": "80",
-    "image": "public/img/products/vlcsnap-2024-09-11-18h39m53s302_VOO1h7O.png"
-  },
-  {
-    "id": 5,
-    "name": "товар 5",
-    "price": 350.0,
-    "article": "100",
-    "image": "public/img/products/vlcsnap-2024-09-11-18h40m58s850.png"
-  },
-  {
-    "id": 6,
-    "name": "товар7",
-    "price": 400.0,
-    "article": "150",
-    "image": "public/img/products/vlcsnap-2024-09-11-18h40m58s850.png"
-  },
-  {
-    "id": 7,
-    "name": "Товар 1",
-    "price": 100.0,
-    "article": "ART-001",
-    "image": "public/img/products/20180915_113204_000019_qTYXR9N.png"
-  },
-  {
-    "id": 8,
-    "name": "Товар 2",
-    "price": 250.0,
-    "article": "ART-002",
-    "image": "no-image.png"
-  },
-  {
     "id": 9,
-    "name": "Товар 3",
-    "price": 50.0,
-    "article": "ART-003",
-    "image": "no-image.png"
+    "name": "Стул Classic",
+    "price": 1200.0,
+    "article": "ART-001",
+    "image": "public/img/products/1.png",
+    "category": "Стулья",
+    "description": "Деревянный стул для дома."
   },
   {
     "id": 10,
-    "name": "Товар 4",
-    "price": 150.0,
-    "article": "ART-004",
-    "image": "no-image.png"
+    "name": "Стол Loft",
+    "price": 8500.0,
+    "article": "ART-002",
+    "image": "public/img/products/2.png",
+    "category": "Столы",
+    "description": "Обеденный стол в стиле Loft."
   },
   {
     "id": 11,
-    "name": "Товар 5",
-    "price": 300.0,
-    "article": "ART-005",
-    "image": "no-image.png"
+    "name": "Шкаф Modern",
+    "price": 15400.0,
+    "article": "ART-003",
+    "image": "public/img/products/3.png",
+    "category": "Шкафы",
+    "description": "Современный вместительный шкаф."
   },
   {
     "id": 12,
-    "name": "Товар 6",
-    "price": 120.0,
-    "article": "ART-006",
-    "image": "no-image.png"
+    "name": "Кровать Dream",
+    "price": 17800.0,
+    "article": "ART-004",
+    "image": "public/img/products/4.png",
+    "category": "Кровати",
+    "description": "Комфортная двуспальная кровать."
   },
   {
     "id": 13,
-    "name": "Товар 7",
-    "price": 220.0,
-    "article": "ART-007",
-    "image": "no-image.png"
+    "name": "Диван Relax",
+    "price": 22300.0,
+    "article": "ART-005",
+    "image": "public/img/products/20180915_113204_000019.png",
+    "category": "Диваны",
+    "description": "Удобный диван для гостиной."
   },
   {
     "id": 14,
-    "name": "Товар 8",
-    "price": 80.0,
-    "article": "ART-008",
-    "image": "no-image.png"
+    "name": "товар",
+    "price": 50.0,
+    "article": "55",
+    "image": "public/img/products/vlcsnap-2024-09-11-18h51m58s188.png",
+    "category": "Стулья",
+    "description": "cc  с с с"
   },
   {
     "id": 15,
-    "name": "Товар 9",
-    "price": 450.0,
-    "article": "ART-009",
-    "image": "no-image.png"
-  },
-  {
-    "id": 16,
-    "name": "Товар 10",
-    "price": 60.0,
-    "article": "ART-010",
-    "image": "no-image.png"
-  },
-  {
-    "id": 17,
-    "name": "товар очень  хороший",
-    "price": 100.0,
-    "article": "450",
-    "image": "public/img/products/vlcsnap-2024-09-11-18h39m53s302_bsFxiXe.png"
-  },
-  {
-    "id": 18,
-    "name": "новый товар",
-    "price": 70.0,
-    "article": "500",
-    "image": "public/img/products/vlcsnap-2024-09-11-18h51m58s188_tlnJkNF.png"
-  },
-  {
-    "id": 19,
-    "name": "нужный товар",
-    "price": 90.0,
-    "article": "90",
-    "image": "public/img/products/20180915_113204_000019_xw8atsH.png"
+    "name": "новая кровать",
+    "price": 7000.0,
+    "article": "7",
+    "image": "public/img/products/vlcsnap-2024-09-11-18h41m29s544.png",
+    "category": "Кровати",
+    "description": "очень  хорошая  кровать"
   }
 ];
 
